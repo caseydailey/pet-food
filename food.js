cef3351d4b47c7e174ea97c1346d07a98340d4da
Casey Dailey <casey@feasttogether.org>
2017-07-27 14:22:02 -0500

    loading dogfoods and displaying

--- food.js
+++ food.js
@@ -1,3 +1,5 @@
+let display = document.querySelector("#foods");
+
 let request = new XMLHttpRequest();
 request.addEventListener("load", handleLoad);
 request.addEventListener("error", handleError);
@@ -6,10 +8,24 @@ request.open("GET", "food.json");
 request.send();
 
 function handleLoad(event){
-    let foods = JSON.parse(this.responseText);
-    console.log("foods", foods);
+    let foods = (JSON.parse(this.responseText)).dog_brands;
+    displayFoods(foods);    
 }
 
 function handleError(event){
     console.log("error", event);
+}
+
+function displayFoods(foodData){
+    foodData.forEach((brand)=>{
+        console.log("brand", brand);
+        display.innerHTML += `  <div class="card">
+                                <h2>${brand.name}</h2>
+                                <h4> type: ${brand.types[0].type}</h4>
+                                <p>sizes for ${brand.types[0].type}:</p>
+                                <li> ${brand.types[0].volumes[0].name}: ${brand.types[0].volumes[0].price}</li> 
+                                <li> ${brand.types[0].volumes[1].name}: ${brand.types[0].volumes[1].price}</li> 
+                                </div>`;
+
+    });
 }
